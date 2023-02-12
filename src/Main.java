b01d2d0cb7900d532a838959959a6c7a3d852143
Mark Owings <owingsmk@mail.uc.edu>
2023-02-12 17:06:49 -0500

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        int num1 = 4; //intital variables
-        int num2 = 18;
+        int num1 = 25; //intital variables
+        int num2 = 8;
         System.out.println("Enter your First Number: "); //Output asking user for the number
         //input of first number
         System.out.println("Enter your Second number: "); //Output asking for second number
