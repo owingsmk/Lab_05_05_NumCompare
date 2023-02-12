f4a5b1f99d2601a675ec968b008c7a9004e40d2e
Mark Owings <owingsmk@mail.uc.edu>
2023-02-12 16:58:34 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num1 = 4; //intiate variables
+        int num2 = 18;
+        System.out.println("Enter your First Number: "); //Output asking user for the number
+        //input of first number
+        System.out.println("Enter your Second number"); //Output asking for second number
+        //input of second number
+
+        if (num1>num2){
+            System.out.println("The larger of " + num1 + "and " + num2 + "is " + num1);
+        }
+    }
+}
