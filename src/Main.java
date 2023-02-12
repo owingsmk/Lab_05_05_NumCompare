8b0d93209202214591b956a5aa333bbf691c10df
Mark Owings <owingsmk@mail.uc.edu>
2023-02-12 17:05:47 -0500

    Third Commit

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ public class Main {
         int num2 = 18;
         System.out.println("Enter your First Number: "); //Output asking user for the number
         //input of first number
-        System.out.println("Enter your Second number"); //Output asking for second number
+        System.out.println("Enter your Second number: "); //Output asking for second number
         //input of second number
 
         if (num1>num2){
-            System.out.println("The larger of " + num1 + "and " + num2 + "is " + num1);
+            System.out.println("The larger of " + num1 + " and " + num2 + " is " + num1);
         } else if (num1<num2) {
-            System.out.println("The larger of " + num2 + "and" + num1 + "is" + num2);
+            System.out.println("The larger of " + num2 + " and " + num1 + " is " + num2);
         } else
-            System.out.println(num1 + num2 "Are equal");
+            System.out.println("Are equal");
     }
 }
